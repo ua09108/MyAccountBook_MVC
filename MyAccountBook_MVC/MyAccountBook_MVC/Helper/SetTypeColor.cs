5d49b31c776d5e42259185bc512c3d28620a003a
ua09108 <ua09018@hotmail.com>
2017-08-22 20:40:12 +0800

    設定支出及收入不同的顏色(使用自訂Html Helper)

---
+++ MyAccountBook_MVC/MyAccountBook_MVC/Helper/SetTypeColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAccountBook_MVC.Helper
+{
+    public static class SetTypeColor
+    {
+        public static MvcHtmlString SetColor(this HtmlHelper helper, string type)
+        {
+            if (type == "收入")
+            {
+                return MvcHtmlString.Create($"<font color='blue'>{type}</font>");
+            }
+            else
+                return MvcHtmlString.Create($"<font color='red'>{type}</font>");
+
+        }
+    }
+}
